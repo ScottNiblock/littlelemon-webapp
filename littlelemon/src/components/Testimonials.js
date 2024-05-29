71ae4c50947ceb7b36d4c34e3e8a3abc4469dfa6
ScottNiblock <scottniblock16@gmail.com>
2024-05-29 14:46:28 -0500

    Reformatted testimonial cards to look better

--- littlelemon/src/components/Testimonials.js
+++ littlelemon/src/components/Testimonials.js
@@ -5,22 +5,26 @@ const users= [
     {
         rating:5,
         image:tom,
-        name:"Tom",
+        name:"Tom B.",
         comments:"Was looking for a quick bite to eat and found this hidden gem, the greek salad is one of the best I have ever had!"
     },
     {
         rating:5,
         image:mary,
-        name:"Mary",
+        name:"Mary H.",
         comments:"I took my kids here and I don't think I have ever seen them enjoy healthy food so much"
     },
     {
         rating:5,
         image:john,
-        name:"John",
+        name:"John D.",
         comments:"The only thing better than the service is the food!"
     },
 ]
+//populate amt of stars based on user rating
+const generateStars = (rating) => {
+    return '⭐'.repeat(rating);
+}
 const Testimonials = () =>{
     return(
         <section className="testimonials">
@@ -29,10 +33,10 @@ const Testimonials = () =>{
             {users.map(user =>{
                 return(
                     <div className="testimonial-card">
-                        <p>{user.name}</p>
-                        <img src={user.image} alt="user image" className="user-img"/>
-                        <p>{user.rating}🌟</p>
+                         <img src={user.image} alt="user image" className="user-img"/>
+                        <p>{generateStars(user.rating)}</p>
                         <p>{user.comments}</p>
+                        <p>- {user.name}</p>
                     </div>
                 )
             })}
